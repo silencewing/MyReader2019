22ea7211c16c56e2a7eed3275bf10a35a3b7d2e2
silencewing <silencewing@163.com>
2019-11-11 17:37:05 +0800

    service to foreground service

--- app/src/main/java/cn/org/silencewing/reader/PlayService.java
+++ app/src/main/java/cn/org/silencewing/reader/PlayService.java
@@ -1,11 +1,16 @@
 package cn.org.silencewing.reader;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.util.Log;
+
+import static android.app.PendingIntent.getActivity;
 
 public class PlayService extends Service {
 
@@ -58,6 +63,8 @@ public class PlayService extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+
 		Log.d(TAG, "PlaybackService::onCreate called");
 
 	/*	mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
@@ -80,7 +87,32 @@ public class PlayService extends Service {
 	}
 
 	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		 Log.d(TAG, "onStartCommand()");
+		 // 在API11之后构建Notification的方式
+		 Notification.Builder builder = new Notification.Builder
+		  (this.getApplicationContext()); //获取一个Notification构造器
+		 Intent nfIntent = new Intent(this, ReadActivity.class);
+
+		 builder.setContentIntent(PendingIntent.
+				getActivity(this, 0, nfIntent, 0)) // 设置PendingIntent
+	//	  .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+	//					   R.mipmap.ic_large)) // 设置下拉列表中的图标(大图标)
+		  .setContentTitle("下拉列表中的Title") // 设置下拉列表里的标题
+	//	  .setSmallIcon(R.mipmap.ic_launcher) // 设置状态栏内的小图标
+		  .setContentText("要显示的内容") // 设置上下文内容
+		  .setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
+
+		 Notification notification = builder.build(); // 获取构建好的Notification
+		 notification.defaults = Notification.DEFAULT_SOUND; //设置为默认的声音
+		startForeground(999, notification);
+		return super.onStartCommand(intent,flags,startId);
+	}
+
+	@Override
 	public void onDestroy() {
+		super.onDestroy();
+		stopForeground(true);// 停止前台服务--参数：表示是否移除之前的通知
 		super.onDestroy();
 
 		/*unregisterRemoteControl();
